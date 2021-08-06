351e45442d7f2c56311d47847d38c32261bd1247
Sumaia Parveen <sumaiaparveen@users.noreply.github.com>
2021-08-06 14:26:12 -0700

    Update app.py

--- Natural Language Processing/Streamlit/app.py
+++ Natural Language Processing/Streamlit/app.py
@@ -24,6 +24,8 @@ import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
 stop_words = stopwords.words('english')
 
+
+
 def time_():
 
     with st.empty():
@@ -696,13 +698,143 @@ def sa_swn(df, col_name):
     data_swn.columns = [[col_name, 'Sentiment']]
     return data_swn
 
+def spacy_sum(text, no_sent):
 
+    import spacy
+    from spacy.lang.en.stop_words import STOP_WORDS
+    from string import punctuation
+    from collections import Counter
+    from heapq import nlargest
+    import nltk
+    import heapq
+    import re
+
+    nlp = spacy.load('en_core_web_sm') # load the model (English) into spaCy
+
+    doc = nlp(text) #pass the string doc into the nlp function.
+    l = len(list(doc.sents)) #number of sentences in the given string
+    st.write('Number of sentences in the original text: {}'.format(l) )
+
+    #def generate_summary(text, no_sent):
+
+    doc = nlp(text)
+    sentence_list=[]
+    for idx, sentence in enumerate(doc.sents): # we are using spacy for sentence tokenization
+        sentence_list.append(re.sub(r'[^\w\s]','',str(sentence)))
+
+    stopwords = nltk.corpus.stopwords.words('english')
+        
+    ## Word frequency 
+    word_frequencies = {}  
+    for word in nltk.word_tokenize(text):  
+        if word not in stopwords:
+            if word not in word_frequencies.keys():
+                word_frequencies[word] = 1
+            else:
+                word_frequencies[word] += 1
+
+    maximum_frequncy = max(word_frequencies.values())
+
+    for word in word_frequencies.keys():  
+        word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+
+    ## Sentence scoring based on word frequency
+    sentence_scores = {}  
+    for sent in sentence_list:  
+        for word in nltk.word_tokenize(sent.lower()):
+            if word in word_frequencies.keys():
+                if len(sent.split(' ')) < 30:
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = word_frequencies[word]
+                    else:
+                        sentence_scores[sent] += word_frequencies[word]
+
+    summary_sentences = heapq.nlargest(int(no_sent), sentence_scores, key=sentence_scores.get)
+    summary = ''.join(summary_sentences)
     
+    return summary
+
+
+def lsa_sum(text, no_sent):
+    import sumy
+    from sumy.parsers.plaintext import PlaintextParser
+    from sumy.nlp.tokenizers import Tokenizer
+    from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+    from sumy.nlp.stemmers import Stemmer
+    from sumy.utils import get_stop_words
+
+    LANGUAGE = "english"
+    SENTENCES_COUNT = int(no_sent)
+    summary = []
+
+    stemmer = Stemmer(LANGUAGE)
+
+    summarizer = Summarizer(stemmer)
+    summarizer.stop_words = get_stop_words(LANGUAGE)
+
+    parser = PlaintextParser.from_string(text, Tokenizer(LANGUAGE))
+
+    ss = []
+
+    for sentence in summarizer(parser.document, SENTENCES_COUNT):
+            ss.append(str(sentence))
+
+    summary = ''.join(ss)
+    return summary
+
+
+def luhn_sum(text, no_sent):
+    # Import the summarizer
+    from sumy.summarizers.luhn import LuhnSummarizer
+
+    # Creating the parser
+    from sumy.nlp.tokenizers import Tokenizer
+    from sumy.parsers.plaintext import PlaintextParser
+    parser=PlaintextParser.from_string(text,Tokenizer('english'))
+
+    SENTENCES_COUNT = int(no_sent)
+
+    #  Creating the summarizer
+    luhn_summarizer=LuhnSummarizer()
+    luhn_summary=luhn_summarizer(parser.document,sentences_count=SENTENCES_COUNT)
+
+    ss = []
+
+    # Printing the summary
+    for sentence in luhn_summary:
+          ss.append(str(sentence))
+            
+    summary = ''.join(ss)
+    return summary
+
+def kl_sum(text, no_sent): 
+    from sumy.summarizers.kl import KLSummarizer
+    # Creating the parser
+    from sumy.nlp.tokenizers import Tokenizer
+    from sumy.parsers.plaintext import PlaintextParser
+    parser=PlaintextParser.from_string(text,Tokenizer('english'))
+
+    SENTENCES_COUNT = int(no_sent)
+
+    # Instantiating the  KLSummarizer
+    kl_summarizer=KLSummarizer()
+    kl_summary=kl_summarizer(parser.document,sentences_count=SENTENCES_COUNT)
+
+    ss = []
+
+    # Printing the summary
+    for sentence in kl_summary:
+        ss.append(str(sentence))
+
+        summary = ''.join(ss)
+        return summary
+
+
 def main():
 
     st.title("Natural Language Processing")
 
-    menu = ["Keyword Extraction", "Topic Modelling", "Sentiment Analysis"]
+    menu = ["Keyword Extraction", "Topic Modelling", "Sentiment Analysis", "Text Summarization"]
     choice = st.sidebar.selectbox("Natural Language Processing", menu)
 
     if choice == "Keyword Extraction":
@@ -1005,7 +1137,6 @@ def main():
 
                     st.subheader("Input DatFrame")
                     st.write(df.head())
-
 
                     if col_name is not None:
 
@@ -1603,6 +1734,66 @@ def main():
                     
                     
                     
+    if choice == "Text Summarization":
+
+        menu_main = ["Extractive Text Summarization", "Abstractive Text Summarization"]
+        choice_main = st.selectbox("Text Summarization", menu_main)
+
+        if choice_main == "Extractive Text Summarization":
+            type_task = st.radio("Select from below", ("spaCy", "Latent Semantic Analysis", "Luhn", "KL-Sum"))
+
+
+            if type_task == 'spaCy':
+                    st.subheader("spaCy")
+
+                    no_sent = st.number_input("How many sentences would you like in the summarized text?")
+                    raw_text = st.text_area("Your Text")
+                    if st.button("Show Summary", key = "321"):
+                        summary = spacy_sum(raw_text, no_sent)
+                        st.info(summary)
+                        #('Download Text', key = "123"):
+                                
+                        tmp_download_link = download_link(summary, 'summary_spacy.txt', 'Download Text')
+                        st.markdown(tmp_download_link, unsafe_allow_html=True)
+
+            if type_task == 'Latent Semantic Analysis':
+                    st.subheader("Sumy: Latent Semantic Analysis")
+
+                    no_sent = st.number_input("How many sentences would you like in the summarized text?")
+                    raw_text = st.text_area("Your Text")
+                    if st.button("Show Summary", key = "321"):
+                        summary = lsa_sum(raw_text, no_sent)
+                        st.info(summary)
+                        #('Download Text', key = "123"):
+                                
+                        tmp_download_link = download_link(summary, 'summary_LSA.txt', 'Download Text')
+                        st.markdown(tmp_download_link, unsafe_allow_html=True)
+
+            if type_task == 'Luhn':
+                    st.subheader("Sumy: Luhn")
+
+                    no_sent = st.number_input("How many sentences would you like in the summarized text?")
+                    raw_text = st.text_area("Your Text")
+                    if st.button("Show Summary", key = "321"):
+                        summary = luhn_sum(raw_text, no_sent)
+                        st.info(summary)
+                        #('Download Text', key = "123"):
+                                
+                        tmp_download_link = download_link(summary, 'summary_Luhn.txt', 'Download Text')
+                        st.markdown(tmp_download_link, unsafe_allow_html=True)
+
+            if type_task == 'KL-Sum':
+                    st.subheader("Sumy: KL-Sum")
+
+                    no_sent = st.number_input("How many sentences would you like in the summarized text?")
+                    raw_text = st.text_area("Your Text")
+                    if st.button("Show Summary", key = "321"):
+                        summary = kl_sum(raw_text, no_sent)
+                        st.info(summary)
+                                
+                        tmp_download_link = download_link(summary, 'summary_KL-Sum.txt', 'Download Text')
+                        st.markdown(tmp_download_link, unsafe_allow_html=True)
+
 
                     
                        
