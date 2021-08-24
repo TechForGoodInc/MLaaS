06c53fe9e5c1dab3e450fdd724330deb5f4b2e6e
Siti Khotijah <53899191+skhotijah@users.noreply.github.com>
2021-08-24 19:37:52 +0700

    Add files via upload

---
+++ Computer Vision/Streamlit/settings.py
@@ -0,0 +1,4 @@
+DEFAULT_CONFIDENCE_THRESHOLD = 0.5
+DEMO_IMAGE = "images/demo.jpg"
+MODEL = r"C:\Users\komp18\Streamlit\data/MobileNetSSD_deploy.caffemodel"
+PROTOTXT = r"C:\Users\komp18\Streamlit\data/MobileNetSSD_deploy.prototxt.txt"
