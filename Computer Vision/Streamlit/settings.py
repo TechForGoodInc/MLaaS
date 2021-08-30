9f875f03c91a20fc8a151c9840c7302790b925ae
Siti Khotijah <53899191+skhotijah@users.noreply.github.com>
2021-08-31 00:02:31 +0700

    Update settings.py

--- Computer Vision/Streamlit/settings.py
+++ Computer Vision/Streamlit/settings.py
@@ -1,4 +1,4 @@
 DEFAULT_CONFIDENCE_THRESHOLD = 0.5
 DEMO_IMAGE = "images/demo.jpg"
-MODEL = r"C:\Users\komp18\Streamlit\data/MobileNetSSD_deploy.caffemodel"
-PROTOTXT = r"C:\Users\komp18\Streamlit\data/MobileNetSSD_deploy.prototxt.txt"
+MODEL = r"C:\Users\skhot\Streamlit\data/MobileNetSSD_deploy.caffemodel"
+PROTOTXT = r"C:\Users\skhot\Streamlit\data/MobileNetSSD_deploy.prototxt.txt"
